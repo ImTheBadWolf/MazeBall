000db19e5fec475ccc3f6709778ac5d0fca765c3
ImTheBadWolf <matus.ozaniak1@gmail.com>
2020-10-20 14:44:57 +0200

    Basic level drawing implemented

--- app/src/main/java/com/example/mazeball/GameActivity.java
+++ app/src/main/java/com/example/mazeball/GameActivity.java
@@ -4,14 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
+
+    SensorManager sensorManager;
+    Sensor gyroscopeSensor;
+
+    game_view gameView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        gameView = (game_view)findViewById(R.id.gameView);
+        gameView.zakric();
     }
+
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(gyroscopeEListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(gyroscopeEListener);
+    }
+    public SensorEventListener gyroscopeEListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int acc) {
+        }
+
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0]*100;
+            float y = event.values[1]*100;
+            Log.d("gyroX", Float.toString(x));
+            Log.d("gyroY", Float.toString(y));
+        }
+    };
+
 
 }
--- app/src/main/java/com/example/mazeball/game_view.java
+++ app/src/main/java/com/example/mazeball/game_view.java
@@ -9,40 +9,46 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
-/**
- * TODO: document your custom view class.
- */
-public class game_view extends View {
+import androidx.appcompat.app.AppCompatActivity;
 
-    Bitmap[] bmp;
+import static android.content.Context.SENSOR_SERVICE;
+
+
+public class game_view extends View{
+
+    Bitmap[] bmp, player;
     int width, height;
-
-    private int[][] emptyMaze = {
-            {1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6},
-            {7,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,9}
+    int gameWidth = 20;
+    int gameHeight = 10;
+    int[][] emptyMaze = {
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,2,0,2,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,2,2,2,2,0,2,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,2,0,2,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
     };
+
+    int[] playerPos = {6,1};
+    int playerSizeMultiplier = 25;
+
+    public void zakric(){
+        Log.d("kric", "yeet");
+    }
+
 
     public game_view(Context context) {
         super(context);
@@ -57,38 +63,35 @@ public class game_view extends View {
     public game_view(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(context);
-        bmp = new Bitmap[10];
-
-        bmp[0] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
-        bmp[1] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
-        bmp[2] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
-        bmp[3] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
-        bmp[4] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
-        bmp[5] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
-        bmp[6] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
-        bmp[7] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
-        bmp[8] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
-        bmp[9] = BitmapFactory.decodeResource(getResources(), R.drawable.eightbutton_background);
     }
 
     private void init(Context context) {
+        bmp = new Bitmap[4];
+        bmp[0] = BitmapFactory.decodeResource(getResources(), R.mipmap.tile5_foreground);
+        bmp[1] = BitmapFactory.decodeResource(getResources(), R.mipmap.tile10_foreground);
+        bmp[2] = BitmapFactory.decodeResource(getResources(), R.mipmap.tile11_foreground);
 
+        player = new Bitmap[1];
+        player[0] = BitmapFactory.decodeResource(getResources(), R.mipmap.player1_foreground);
     }
+
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        width = w / 20;
-        height = h / 20;
+        width = w / gameWidth;
+        height = h / gameHeight;
         super.onSizeChanged(w, h, oldw, oldh);
     }
     @Override
     protected void onDraw(Canvas canvas) {
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                canvas.drawBitmap(bmp[emptyMaze[i][j]], null,
-                        new Rect(j * width, i * height, (j + 1) * width, (i + 1) * height), null);
+
+        for (int i = 0; i < gameHeight; i++) {
+            for (int j = 0; j < gameWidth; j++) {
+                canvas.drawBitmap(bmp[emptyMaze[i][j]], null, new Rect(j * width, i * height, (j + 1) * width, (i + 1) * height), null);
             }
         }
+        canvas.drawBitmap(player[0], null, new Rect(playerPos[0] * width-playerSizeMultiplier, playerPos[1] * height-playerSizeMultiplier, (playerPos[0] + 1) * width+playerSizeMultiplier, (playerPos[1] + 1) * height+playerSizeMultiplier), null);
+
     }
 }
 
